802b0bf102fbe0b5c8746f98d78e240ae9a7a309
Nan R <nanbusiness13@gmail.com>
2022-01-15 12:58:05 +0100

    🚑️ Pos is now updated in the main loop
    It wasn't being updated properly after each command executed, leading to an infinite loop of despair that continuously filled my memory and never properly ended.

--- source/main.py
+++ source/main.py
@@ -6,7 +6,7 @@ temp:str = None
 TOKENS:list = []
 COLOURS  = {'RED', 'BLUE', 'PURPLE', 'GREEN', 'YELLOW', 'CYAN', 'BLACK', 'WHITE', 'BROWN', 'LIME', 'PINK', 'ORANGE'}
 COMMANDS = {'VENTED', 'SUSSY', 'ELECTRICAL'}
-with open(fileaaa, 'r') as f:
+with open(r'C:\Users\Nan\Documents\Among-us-interpreter-py\Among-us-interpreter-py\source\_filename.sus', 'r') as f:
     TEXT = f.read()
 
 for a in TEXT.split():
@@ -41,39 +41,51 @@ while pos < len(TOKENS):
         colour_command = current.replace('COL_', '')
         if colour_command == 'RED':
             command.red()
+            pos += 1
             continue
         elif colour_command == 'BLUE':
             command.blue()
+            pos += 1
             continue
         elif colour_command == 'PURPLE':
             command.purple()
+            pos += 1
             continue
         elif colour_command == 'GREEN':
             command.green()
+            pos += 1
             continue
         elif colour_command == 'YELLOW':
             command.yellow()
+            pos += 1
             continue
         elif colour_command == 'CYAN':
             command.cyan()
+            pos += 1
             continue
         elif colour_command == 'BLACK':
             command.black()
+            pos += 1
             continue
         elif colour_command == 'WHITE':
             command.white()
+            pos += 1
             continue
         elif colour_command == 'BROWN':
             command.brown()
+            pos += 1
             continue
         elif colour_command == 'LIME':
             command.lime()
+            pos += 1
             continue
         elif colour_command == 'PINK':
             command.pink()
+            pos += 1
             continue
         elif colour_command == 'ORANGE':
             command.orange()
+            pos += 1
             continue
         else:
             print(f'Unexpected error at TOKENS[{pos}] trying to execute {current}. \n\tCommand type: COL')
@@ -83,12 +95,15 @@ while pos < len(TOKENS):
         cmd_command = current.replace('CMD_', '')
         if cmd_command == 'VENTED':
             command.vented()
+            pos += 1
             continue
         elif cmd_command == 'SUSSY':
             command.sussy()
+            pos += 1
             continue
         elif cmd_command == 'ELECTRICAL':
             command.electrical()
+            pos += 1
             continue
         else:
             print(f'Unexpected error at TOKENS[{pos}] trying to execute {current}. \n\tCommand type: CMD')
@@ -96,6 +111,9 @@ while pos < len(TOKENS):
     
     elif current.startswith('START_'):
         loop_starter = pos
+        pos += 1
         continue
+    
+    else:
+        pos += 1
 
-    pos += 1
